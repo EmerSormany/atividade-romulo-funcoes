f4fa1271baabd934c464a47c2b496d4417f45fbe
Emerson Sormany <sormanyemerson@gmail.com>
2024-12-06 18:14:43 +0000

    resolução parcial

---
+++ exercicios.py
@@ -0,0 +1,37 @@
+
+
+def saudacao(name):
+    """
+    Escreva uma função chamada saudacao que aceita um nome como argumento e imprime "Olá, [nome]!".
+    """
+    return f'Olá, {name}!'
+
+def dobro(x):
+    """
+    Crie uma função dobro que aceita um número como argumento e retorna o dobro desse número.
+    """
+    return x*2
+
+def saudacao_personalizada(name, language):
+    '''
+    Crie uma função chamada saudacao_personalizada que aceita um nome e um idioma como argumentos. O idioma é opcional e padrão para "inglês". A função deve retornar uma saudação no idioma especificado.
+    '''
+    if language == 'inglês':
+        return f'Hello, {name}!'
+    elif language == 'espanhol':
+        return f'Hola, {name}!'
+    else:
+        return f'Bonjuor, {name}!'
+
+def potencia(base, exhibitor = 2):
+    '''
+    Escreva uma função potencia que aceita uma base e um expoente (com expoente padrão igual a 2) e retorna a base elevada ao expoente
+    '''
+    return base ** exhibitor
+
+    
+# Defina uma função chamada potencia que calcula a potência de um número elevado a uma potência especificada. Forneça uma documentação que explique como usar a função e inclua exemplos de uso.
+def 
+    '''
+    
+    '''
